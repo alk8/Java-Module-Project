c713384b1902cb1802b14508e1a2dd18142f10d4
alk8 <l3pxbsk2kh>
2022-08-12 14:35:21 +0300

    Первый кусок

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,15 +1,17 @@
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Counter {
 
     private int countPerson = 0;
-    private HashMap<String,Double> goods = new HashMap<String,Double>();
+    private HashMap<String, Double> goods = new HashMap<String, Double>();
 
-    Counter(int countPerson){
+    Counter(int countPerson) {
         this.countPerson = countPerson;
     }
 
-    public void askUser(){
+    public void askUser() {
 
         final String regex = "'.*?' [0-9]{2}.[0-9]{2}";
 
@@ -18,35 +20,45 @@ public class Counter {
         System.out.println("Введите товар в формате \"'рубли.копейки' [10.45, 11.40]\"");
 
         // Получение ввода, парсинг
-        String[] input = sc.next(regex).split(" ");
+        String input = sc.nextLine();
 
-        // Попытка добавления товара
-         if (addGoods(input)) {
+        if (input.matches(regex)) {
 
-            // Спросить о продолжении сессии
-            if (endSession(sc)) {
-                System.out.println("Сессия закончена. Спасибо!");
-                return;
+            String[] inputArray = input.split(" ");
+
+            // Попытка добавления товара
+            if (addGoods(inputArray)) {
+
+                // Спросить о продолжении сессии
+                if (endSession(sc)) {
+                    System.out.println("Сессия закончена. Спасибо!");
+                    return;
+                }
+
+                // Снова спрашиваем про товар (Рекурсия)
+                askUser();
+
             }
 
-            // Снова спрашиваем про товар (Рекурсия)
+        }else {
+            // Не соответсвует маске
+            System.out.println("Введенный текст не соответсвует маске.");
             askUser();
-
         }
 
     }
 
-    private boolean endSession(Scanner sc){
+    private boolean endSession(Scanner sc) {
 
         return sc.next().toUpperCase() == "ЗАВЕРШИТЬ";
 
     }
 
-    private boolean addGoods(String[] input){
+    private boolean addGoods(String[] input) {
 
         boolean success = true;
 
-        goods.put(input[0],new Double(input[0]));
+        goods.put(input[0], new Double(input[0]));
 
         return success;
 
